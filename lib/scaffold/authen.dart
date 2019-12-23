37ce3549b22b6872cd896f1fb50daa673c251ff3
piyaporn557 <p.jongjaijit@gmail.com>
2019-12-23 14:23:33 +0700

    Design UI

--- lib/scaffold/authen.dart
+++ lib/scaffold/authen.dart
@@ -9,10 +9,62 @@ class _AuthenState extends State<Authen> {
   // Field
 
   // Method
-  Widget userForm(){
-    return TextFormField();
+  Widget mySizebox() {
+    return SizedBox(
+      width: 5.0,
+      height: 16.0,
+    );
   }
 
+  Widget signInButton() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      color: Colors.cyan.shade700,
+      child: Text(
+        'Sing In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: Text('Sing Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        mySizebox(),
+        signUpButton(),
+      ],
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'User :'),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Password :'),
+      ),
+    );
+  }
 
   Widget showLogo() {
     return Container(
@@ -38,14 +90,26 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            showAppName(),
-            userForm(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+          colors: <Color>[Colors.white, Colors.blue.shade700],
+          radius: 1.0,
+        )),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                mySizebox(),
+                showAppName(),
+                userForm(),
+                passwordForm(),
+                showButton(),
+              ],
+            ),
+          ),
         ),
       ),
     );
