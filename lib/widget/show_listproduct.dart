013db30bd2f3c414902cd483014664b87af15562
piyaporn557 <p.jongjaijit@gmail.com>
2019-12-25 16:17:32 +0700

    25 Dec

--- lib/widget/show_listproduct.dart
+++ lib/widget/show_listproduct.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:ketot/models/product_model.dart';
 import 'package:ketot/models/user_model.dart';
 import 'package:ketot/scaffold/add_new_product.dart';
 import 'package:ketot/utility/my_style.dart';
 
 class ShowListProduct extends StatefulWidget {
-
   final UserModel userModel;
-  ShowListProduct({Key key, this.userModel}):super(key: key);
+  ShowListProduct({Key key, this.userModel}) : super(key: key);
 
   @override
   _ShowListProductState createState() => _ShowListProductState();
@@ -15,12 +18,87 @@ class ShowListProduct extends StatefulWidget {
 class _ShowListProductState extends State<ShowListProduct> {
   // Field
   UserModel myUsermodel;
+  List<ProductModel> productModels = List();
 
   // Method
   @override
-  void initState(){
+  void initState() {
     super.initState();
     myUsermodel = widget.userModel;
+    readAllProductThread();
+  }
+
+  Future<void> readAllProductThread() async {
+    String url = 'http://androidthai.in.th/tot/getAllProductKea.php';
+
+    Response response = await Dio().get(url);
+    var result = json.decode(response.data);
+    print('result ====>>> $result');
+
+    for (var map in result) {
+      ProductModel productModel = ProductModel.fromJson(map);
+      setState(() {
+        productModels.add(productModel);
+      });
+    }
+  }
+
+  Widget showImage(int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.4,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(productModels[index].path),fit: BoxFit.fitWidth,
+        ),
+      ),
+    );
+  }
+
+  Widget showProduct(int index) {
+    return Text(
+      productModels[index].product,
+      style: MyStyle().h2Style,
+    );
+  }
+
+  Widget showDetail(int index) {
+    String string = productModels[index].detail;
+    if (string.length >= 40) {
+      string = string.substring(0, 39);
+      string = '$string ...';
+    }
+
+    return Text(string);
+  }
+
+  Widget showText(int index) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.height * 0.4,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          showProduct(index),
+          showDetail(index),
+        ],
+      ),
+    );
+  }
+
+  Widget showListView() {
+    return ListView.builder(
+      itemCount: productModels.length,
+      itemBuilder: (BuildContext buildContext, int index) {
+        return Row(
+          children: <Widget>[
+            showImage(index),
+            showText(index),
+          ],
+        );
+      },
+    );
   }
 
   Widget addProductButton() {
@@ -39,7 +117,9 @@ class _ShowListProductState extends State<ShowListProduct> {
                   ////กดแล้วเปลี่ยนหน้า
                   MaterialPageRoute materialPageRoute =
                       MaterialPageRoute(builder: (BuildContext buildContext) {
-                    return AddNewProduct(userModel: myUsermodel,);
+                    return AddNewProduct(
+                      userModel: myUsermodel,
+                    );
                   });
                   Navigator.of(context).push(materialPageRoute);
                 },
@@ -57,6 +137,7 @@ class _ShowListProductState extends State<ShowListProduct> {
       child: Stack(
         children: <Widget>[
           addProductButton(),
+          showListView(),
         ],
       ),
     );
